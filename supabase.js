1dfc6267853e4754b2bdd5d2ad3562f470e7c898
narayananselvitravels <narayananselvitravels@gmail.com>
2025-07-11 19:23:59 +0530

    Update and rename firebase.js to supabase.js

---
+++ supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+export const supabase = createClient(
+  'https://https://dqexkvjohbcoezthucsk.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxZXhrdmpvaGJjb2V6dGh1Y3NrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIyNDEwMDIsImV4cCI6MjA2NzgxNzAwMn0.io-bYY7fFIPPACmyVNXW8326YF-cVVtUgqM9F5n6Tks'
+);
